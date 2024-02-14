2f1cb3d3a2237c43d860063ec0445e698ccebb54
Ametsa <125867374+ametsa77@users.noreply.github.com>
2024-02-14 20:47:35 +0300

    Update Webhook.hpp
    removed expired webhook link

--- Webhook.hpp
+++ Webhook.hpp
@@ -11,5 +11,5 @@ public:
 	void message(const char* message);
 	const char* get_url() { return url; }
 private:
-	const char* url = "https://discord.com/api/webhooks/1206612183509762079/Y2axClA-oykuX3wwHs0_vwv7tWtXoft42-aGeVmXn2hRKq1QsoNEJYwh99_Iq9D29eA7";
+	const char* url = "";
 };
